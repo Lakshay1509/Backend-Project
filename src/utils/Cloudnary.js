0036fc36416c8888ecf0d72f93f2c521db081a4f
Lakshay Gupta <gupta15.lakshay@gmail.com>
2024-05-21 13:40:26 +0530

    added config for file upload

---
+++ src/utils/Cloudnary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const upoadOnCloudinary = async (loacalfilePath) => {
+
+
+    try {
+        if (!loacalfilePath) return null;
+        const response = await cloudinary.uploader.upload(loacalfilePath, {
+            resource_type: "auto",
+        })
+
+        console.log("Uplaod Successfully", response.url)
+        return response;
+    } catch (err) {
+
+        fs.unlinkSync(loacalfilePath);
+        return null;
+
+    }
+
+
+}
+
+export {upoadOnCloudinary}
